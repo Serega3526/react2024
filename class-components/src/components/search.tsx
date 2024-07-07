a99b3ed6da8fea986f11e0b979536aa76f721767
Sergey Kruglov <serega-kolpino@inbox.ru>
2024-07-07 21:41:55 +0300

    feat: add top and bottom item, get api date

---
+++ class-components/src/components/search.tsx
@@ -0,0 +1,15 @@
+import { Component, ReactNode } from "react";
+
+
+class Search extends Component {
+    render(): ReactNode {
+        return (
+            <div>
+                <input type="search" />
+                <button>Найти</button>
+            </div>
+        )
+    }
+}
+
+export default Search
---
+++ class-components/src/components/topLine.tsx
@@ -0,0 +1,15 @@
+import { Component, ReactNode } from "react";
+import Search from "./search";
+
+
+class TopLine extends Component {
+    render(): ReactNode {
+        return (
+            <div className="topLine">
+                <Search/>
+            </div>
+        )
+    }
+}
+
+export default TopLine
